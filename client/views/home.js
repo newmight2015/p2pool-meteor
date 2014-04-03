22a81c1d6529946bfb9bcffec1c55b445b52b707
Jonathan Pidgeon <jon@empire5design.com>
2014-04-03 01:57:11 -0600

    added pings

--- client/views/home.js
+++ client/views/home.js
@@ -7,20 +7,47 @@ Template.home.helpers({
 
 Template.node.rendered = function() {
   var self = this;
+  ping(this.data, function(err, res) {
   Meteor.setTimeout(function() {
+    $(self.find('.blast')).append('  <span class="label label-default">'+res+'</span>');
     $(self.find('.blast')).removeClass('blast');
   }, 500);
+  });
 };
 
 Template.home.events({
   'click button': function(event, template) {
-    var ip = $('input').val();
-    var currency = $(event.target).attr('id');
-    Session.set('ip', ip);
-    Meteor.call('scan', ip, currency, function(err, res) {
-      if(!err) {
-        console.log('boop')
-      }
-    });
+    if($(event.target).attr('id') === 'nuke') {
+      Meteor.call('nuke');
+    } else {
+      var ip = $('#ip').val();
+      var port = $('#port').val();
+      var currency = $(event.target).attr('id');
+      Session.set('ip', ip);
+      Meteor.call('scan', ip, port, currency, function(err, res) {
+        if(!err) {
+          console.log('boop')
+        }
+      });
+    }
   }
-})
+});
+
+function ping(ip, callback) {
+  var now = +new Date;
+  var ws = new WebSocket("ws://" + ip);
+  ws.onerror = function(e){
+    var later = +new Date;
+    var ms = later-now;
+    callback(null, ms);
+    ws = null;
+  };
+  setTimeout(function() {
+    if(ws != null) {
+      ws.close();
+      ws = null;
+      callback(true, null);
+    }
+  },2000);
+}
+
--- server/server.js
+++ server/server.js
@@ -1,32 +1,49 @@
 var currencies = {
   'drk': 7903,
   'fst': 5150,
-  'btc': 9332
+  'btc': 9332,
+  'ltc': 9327
 };
 
 
 Meteor.methods({
-  scan: function(ip, currency) {
-    //todo: change port based on currency
-    var port = currencies[currency];
+  scan: function(ip, port, currency) {
+    if(_.isEmpty(port)) {
+      port = currencies[currency];
+    }
     HTTP.get('http://'+ip+':'+port+'/peer_addresses', function(err, res) {
       if(!err && res.data) {
         var nodes = res.data.split(' ');
+        nodes = stripPorts(nodes);
         console.log(nodes)
-        Pools.upsert({seed: ip}, {seed: ip, nodes: nodes});
+        Pools.upsert({seed: ip}, {seed: ip, nodes: nodes, found: true});
         _.each(nodes, function(node) {
-          var noPort = node.substring(0, node.indexOf(':'));
-          HTTP.get('http://'+noPort+':'+port+'/peer_addresses', function(err, res) {
+          var secondaryPort = currencies[currency];
+          HTTP.get('http://'+node+':'+secondaryPort+'/peer_addresses', function(err, res) {
             if(!err && res.data) {
               var newNodes= res.data.split(' ');
+              newNodes = stripPorts(newNodes);
               console.log(newNodes)
               Pools.update({seed: ip}, {$addToSet: {nodes: {$each: newNodes}}})
             }
           })
         })
       } else {
-        console.log(err)
+        console.log('error:' + err)
+        Pools.upsert({seed: ip}, {seed: ip, found: false});
       }
     })
+  },
+  nuke: function() {
+    Pools.remove({});
   }
 });
+
+var stripPorts = function(arr) {
+  var portless = [];
+  for(var i = 0, newNode; newNode = arr[i++];) {
+    newNode = newNode.substring(0, newNode.indexOf(':'));
+    portless.push(newNode);
+  }
+  return portless;
+}
